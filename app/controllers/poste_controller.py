762565393fcb1f363ba1daa3c946fe01a1c181b5
alexandre <alexandre.deloire01@etu.umontpellier.fr>
2024-03-21 19:47:42 +0100

    referents by poste

--- app/controllers/poste_controller.py
+++ app/controllers/poste_controller.py
@@ -40,4 +40,27 @@ async def delete_poste(festival_id: int, poste: str):
         result = await db.execute(query, festival_id, poste)
     
         return { "message" :"Poste deleted successfully" }
+
+
+# Function to get the list of referents for a poste
+async def get_referents_for_poste(festival_id: int, poste: str):
     
+        query = """
+        SELECT 
+                ARRAY_AGG(users.user_id) as user_ids,
+                ARRAY_AGG(users.username) as usernames,
+                postes.poste_id,
+                postes.poste,
+                postes.description_poste,
+                postes.max_capacity
+        FROM users
+        JOIN referents ON users.user_id = referents.user_id
+        JOIN postes ON referents.poste_id = postes.poste_id
+        WHERE postes.festival_id = $1 AND referents.festival_id = $2 AND postes.poste = $3
+	GROUP BY postes.poste, postes.poste_id, postes.description_poste, postes.max_capacity;"""
+    
+        result = await db.fetch_rows(query, festival_id, festival_id, poste)
+        
+        result = [dict(row) for row in result]
+        
+        return result
